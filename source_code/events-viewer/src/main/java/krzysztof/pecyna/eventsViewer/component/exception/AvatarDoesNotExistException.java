bc4d3032167f5705f16ac496bf6fafd8b33cae54
Krzysztof Pecyna <krzysztofpe-01@o2.pl>
2024-10-14 11:17:34 +0200

    Added exceptions.

---
+++ source_code/events-viewer/src/main/java/krzysztof/pecyna/eventsViewer/component/exception/AvatarDoesNotExistException.java
@@ -0,0 +1,7 @@
+package krzysztof.pecyna.eventsViewer.component.exception;
+
+public class AvatarDoesNotExistException extends Exception{
+    public AvatarDoesNotExistException(String message) {
+        super(message);
+    }
+}
---
+++ source_code/events-viewer/src/main/java/krzysztof/pecyna/eventsViewer/component/exception/AvatarExistsException.java
@@ -0,0 +1,7 @@
+package krzysztof.pecyna.eventsViewer.component.exception;
+
+public class AvatarExistsException extends Exception{
+    public AvatarExistsException(String message) {
+        super(message);
+    }
+}
